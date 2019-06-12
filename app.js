027e5aa574ea785d17b39cb4cb3c72fcadf63093
zerolux-e <51708943+zerolux-e@users.noreply.github.com>
2019-06-12 09:01:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -496,27 +496,3 @@ google.maps.event.addListener(marker, 'click', function(e) {
 
   return heatmapData;
 }
-
-
-$(function() {
-/**
-* Smooth scrolling to page anchor on click
-**/
-$("a[href*='#']:not([href='#'])").click(function() {
-    if (
-        location.hostname == this.hostname
-        && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-    ) {
-        var anchor = $(this.hash);
-        anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-        if ( anchor.length ) {
-            $("html, body").animate( { scrollTop: anchor.offset().top - 30 }, 400);
-        }
-    }
-});
-});
-    
-$(window).on('load resize' , function(e){
-    var headerHeight = $('#header').height();
-    $('#primary').animate({"color":"#e8a010"},400);
-      });
