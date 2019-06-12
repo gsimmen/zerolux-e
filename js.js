e7d1dc5a956b94f7f3715f61b42cba73e1ee36db
zerolux-e <51708943+zerolux-e@users.noreply.github.com>
2019-06-12 09:03:00 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+
+$(function() {
+/**
+* Smooth scrolling to page anchor on click
+**/
+$("a[href*='#']:not([href='#'])").click(function() {
+    if (
+        location.hostname == this.hostname
+        && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+    ) {
+        var anchor = $(this.hash);
+        anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+        if ( anchor.length ) {
+            $("html, body").css( { scrollTop: anchor.offset().top - 30 });
+        }
+    }
+});
+});
+
+
+var headerHeight = $('#header').height();
+$(window).on('load resize' , function(){
+    $('#primary').animate({"height" : "calc(100vh - " + headerHeight + ")" - 10 }, 400);
+      });
+
+
+//
+//
+});//jquery function
