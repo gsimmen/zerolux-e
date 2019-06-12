e7b2c24a4560a28bb92e015b9905f67b9e2638a4
zerolux-e <51708943+zerolux-e@users.noreply.github.com>
2019-06-12 10:01:25 +0200

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+
+$(function() {
+/**
+* Smooth scrolling to page anchor on click
+**/
+$("a[href*='#']:not([href='#'])").click(function() {
+    if (
+        location.hostname == this.hostname
+        && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+    ) {
+        var anchor = $(this.hash);
+        var headerHeight = $('#header').outerHeight();
+        anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+        if ( anchor.length ) {
+            $("html, body").animate( { scrollTop: anchor.offset().top + headerHeight }, 400);
+        }
+    }
+});
+});
+
+//$(window).on('load resize' , function(){
+//var headerHeight = $('#header').height();
+    //$('#primary').animate({"height" : "calc(100vh - " + headerHeight + ")" - 10 }, 400);
+      //});
+
+
+//
+//
+});//jquery function
